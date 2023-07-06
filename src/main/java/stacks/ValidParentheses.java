ee1f09d1757df63ab49d1b258251237c520ddfbf
Dmitri Butorchin <Dmitri.Butorchin@gmail.com>
2023-07-06 23:59:23 +0300

    valid parentheses

---
+++ src/main/java/stacks/ValidParentheses.java
@@ -0,0 +1,42 @@
+package stacks;
+
+import java.util.Stack;
+
+/**
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+ * determine if the input string is valid.
+ * <p>
+ * An input string is valid if:
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ */
+public class ValidParentheses {
+    private static final char OPEN_ROUND = '(';
+    private static final char CLOSE_ROUND = ')';
+    private static final char OPEN_SQUIGGLY = '{';
+    private static final char CLOSE_SQUIGGLY = '}';
+    private static final char OPEN_SQUARE = '[';
+    private static final char CLOSE_SQUARE = ']';
+
+    public boolean isValid(String s) {
+        var stack = new Stack<Character>();
+        for (var character : s.toCharArray()) {
+            if (OPEN_ROUND == character || OPEN_SQUIGGLY == character || OPEN_SQUARE == character) {
+                stack.push(character);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                var open = stack.pop();
+                if ((OPEN_ROUND == open && CLOSE_ROUND != character)
+                        || (OPEN_SQUIGGLY == open && CLOSE_SQUIGGLY != character)
+                        || (OPEN_SQUARE == open && CLOSE_SQUARE != character)) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
---
+++ src/test/java/stacks/ValidParenthesesTest.java
@@ -0,0 +1,58 @@
+package stacks;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ValidParenthesesTest {
+    ValidParentheses solver;
+
+    @BeforeEach
+    void setUp() {
+        solver = new ValidParentheses();
+    }
+
+    @Test
+    void test1() {
+        var actual = solver.isValid("()");
+        assertTrue(actual);
+    }
+
+    @Test
+    void test2() {
+        var actual = solver.isValid("()[]{}");
+        assertTrue(actual);
+    }
+
+    @Test
+    void test3() {
+        var actual = solver.isValid("(]");
+        assertFalse(actual);
+    }
+
+    @Test
+    void test4() {
+        var actual = solver.isValid("([]({})[])");
+        assertTrue(actual);
+    }
+
+    @Test
+    void test5() {
+        var actual = solver.isValid("][]");
+        assertFalse(actual);
+    }
+
+    @Test
+    void test6() {
+        var actual = solver.isValid("[]]");
+        assertFalse(actual);
+    }
+
+    @Test
+    void test7() {
+        var actual = solver.isValid("[][");
+        assertFalse(actual);
+    }
+}
