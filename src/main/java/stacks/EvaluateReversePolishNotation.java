8cf3f0056b974a5e920304e610c0650dbc720842
Dmitri Butorchin <Dmitri.Butorchin@gmail.com>
2023-07-07 14:47:51 +0300

    evaluate reverse polish notation

---
+++ src/main/java/stacks/EvaluateReversePolishNotation.java
@@ -0,0 +1,39 @@
+package stacks;
+
+import java.util.Stack;
+
+/**
+ * You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+ * <p>
+ * Evaluate the expression. Return an integer that represents the value of the expression.
+ * <p>
+ * Note that:
+ * <p>
+ * The valid operators are '+', '-', '*', and '/'.
+ * Each operand may be an integer or another expression.
+ * The division between two integers always truncates toward zero.
+ * There will not be any division by zero.
+ * The input represents a valid arithmetic expression in a reverse polish notation.
+ * The answer and all the intermediate calculations can be represented in a 32-bit integer.
+ */
+public class EvaluateReversePolishNotation {
+    public int evalRPN(String[] tokens) {
+        var stack = new Stack<Integer>();
+        for (var token : tokens) {
+            if ("+".equals(token) || "-".equals(token) || "*".equals(token) || "/".equals(token)) {
+                var second = stack.pop();
+                var first = stack.pop();
+                switch (token) {
+                    case "+" -> stack.push(first + second);
+                    case "-" -> stack.push(first - second);
+                    case "*" -> stack.push(first * second);
+                    case "/" -> stack.push(first / second);
+                }
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+}
---
+++ src/test/java/stacks/EvaluateReversePolishNotationTest.java
@@ -0,0 +1,33 @@
+package stacks;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class EvaluateReversePolishNotationTest {
+    EvaluateReversePolishNotation solver;
+
+    @BeforeEach
+    void setUp() {
+        solver = new EvaluateReversePolishNotation();
+    }
+
+    @Test
+    void test1() {
+        var actual = solver.evalRPN(new String[]{"2", "1", "+", "3", "*"});
+        assertEquals(9, actual);
+    }
+
+    @Test
+    void test2() {
+        var actual = solver.evalRPN(new String[]{"4", "13", "5", "/", "+"});
+        assertEquals(6, actual);
+    }
+
+    @Test
+    void test3() {
+        var actual = solver.evalRPN(new String[]{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"});
+        assertEquals(22, actual);
+    }
+}
