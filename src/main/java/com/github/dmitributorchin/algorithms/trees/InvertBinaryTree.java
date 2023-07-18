f92caa8cb530a1db203c0d76c662cc9012c25099
Dmitri Butorchin <Dmitri.Butorchin@gmail.com>
2023-07-18 19:04:07 +0300

    invert binary tree

---
+++ src/main/java/com/github/dmitributorchin/algorithms/trees/InvertBinaryTree.java
@@ -0,0 +1,27 @@
+package com.github.dmitributorchin.algorithms.linkedlist;
+
+/**
+	Given the root of a binary tree, invert the tree, and return its root.
+ */
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            var node = queue.remove();
+            if (node != null) {
+                var left = node.left;
+                node.left = node.right;
+                node.right = left;
+                queue.add(node.left);
+                queue.add(node.right);
+            }
+        }
+
+        return root;
+    }
+}
